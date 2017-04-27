c922e9c7348379072a73c2b9534a31b3698d741a
Oleg Obleukhov <oleg.obleukhov@innogames.com>
2017-04-27 18:24:46 +0200

    Bots are smarter

--- crashci.go
+++ crashci.go
@@ -62,6 +62,7 @@ const (
 // Colors
 const (
 	RESET = 0
+	BOLD  = 1
 	RED   = 31 + iota
 	GREEN
 	YELLOW
@@ -109,6 +110,7 @@ type Round struct {
 	Players         []Player
 	State           int
 	LastStateChange time.Time
+	Bonus           Point
 	FrameBuffer     Symbols
 }
 
@@ -238,6 +240,59 @@ func checkRoundRun(runningRoundChannel chan Round) {
 	}
 }
 
+func (round *Round) applyBonuses(activeMap []Symbol) {
+	if round.State == FINISHED {
+		return
+	} else if round.Bonus.X == -1 && round.Bonus.Y == -1 {
+		if rand.Int()%100 == 0 {
+			round.Bonus = Point{rand.Intn(mapWidth-nameTableWidth-1) + 1, rand.Intn(mapHeight-1) + 1}
+		}
+		//activeMap[(line+1)*mapWidth+(mapWidth-3)-len(health)+i] = Symbol{player.Color, []byte{char}}
+	}
+
+}
+
+func (round *Round) applyHealth(activeMap []Symbol) {
+	for line, player := range round.Players {
+		health := []byte(fmt.Sprintf("%3d", player.Health))
+		for i, char := range health {
+			activeMap[(line+1)*mapWidth+(mapWidth-3)-len(health)+i] = Symbol{player.Color, []byte{char}}
+		}
+	}
+}
+
+func (round *Round) applyCars(activeMap []Symbol) {
+	for _, player := range round.Players {
+		charPosX, charPosY := 0, 0
+		for i := 0; i < len(cars[player.Car.Direction]); i++ {
+			var chars []byte
+			if cars[player.Car.Direction][i] == byte('\n') {
+				charPosY++
+				charPosX = 0
+				continue
+			} else if cars[player.Car.Direction][i] == 226 {
+				/*
+				 This means extended ASCII is used. After 226 2 bytes must follow
+				*/
+				chars = []byte{cars[player.Car.Direction][i], cars[player.Car.Direction][i+1], cars[player.Car.Direction][i+2]}
+				i += 2
+			} else if cars[player.Car.Direction][i] == 194 {
+				/*
+				 This means extended ASCII is used. After 194 1 bytes must follow
+				*/
+				chars = []byte{cars[player.Car.Direction][i], cars[player.Car.Direction][i+1]}
+				i++
+			} else if player.Health <= 0 && cars[player.Car.Direction][i] == 'o' {
+				chars = []byte{'x'}
+			} else {
+				chars = []byte{cars[player.Car.Direction][i]}
+			}
+			activeMap[(player.Car.Borders.Points[LEFTUP].Y+charPosY)*mapWidth+player.Car.Borders.Points[LEFTUP].X+charPosX] = Symbol{player.Color, chars}
+			charPosX++
+		}
+	}
+}
+
 func (rectangle *Rectangle) intersects(r *Rectangle) bool {
 	if rectangle.Points[RIGHTDOWN].X < r.Points[LEFTUP].X ||
 		r.Points[RIGHTDOWN].X < rectangle.Points[LEFTUP].X ||
@@ -246,6 +301,28 @@ func (rectangle *Rectangle) intersects(r *Rectangle) bool {
 		return false
 	}
 	return true
+}
+
+func (rectangle *Rectangle) nextTo(r *Rectangle) int {
+	// Return the closes side of rectangles
+	if rectangle.Points[LEFTUP].X-3 <= r.Points[RIGHTDOWN].X && rectangle.Points[LEFTUP].X-3 >= r.Points[LEFTDOWN].X &&
+		((rectangle.Points[LEFTUP].Y <= r.Points[RIGHTDOWN].Y && rectangle.Points[LEFTUP].Y >= r.Points[RIGHTUP].Y) ||
+			(rectangle.Points[LEFTDOWN].Y <= r.Points[RIGHTDOWN].Y && rectangle.Points[LEFTDOWN].Y >= r.Points[RIGHTUP].Y)) {
+		return LEFT
+	} else if rectangle.Points[RIGHTUP].X+3 >= r.Points[LEFTDOWN].X && rectangle.Points[RIGHTUP].X+3 <= r.Points[RIGHTDOWN].X &&
+		((rectangle.Points[LEFTUP].Y <= r.Points[RIGHTDOWN].Y && rectangle.Points[LEFTUP].Y >= r.Points[RIGHTUP].Y) ||
+			(rectangle.Points[LEFTDOWN].Y <= r.Points[RIGHTDOWN].Y && rectangle.Points[LEFTDOWN].Y >= r.Points[RIGHTUP].Y)) {
+		return RIGHT
+	} else if rectangle.Points[LEFTUP].Y-3 <= r.Points[RIGHTDOWN].Y && rectangle.Points[LEFTUP].Y-3 >= r.Points[RIGHTUP].Y &&
+		((rectangle.Points[LEFTUP].X <= r.Points[RIGHTDOWN].X && rectangle.Points[LEFTUP].X >= r.Points[LEFTDOWN].X) ||
+			(rectangle.Points[RIGHTUP].X >= r.Points[LEFTDOWN].X && rectangle.Points[RIGHTUP].X <= r.Points[RIGHTDOWN].X)) {
+		return UP
+	} else if rectangle.Points[RIGHTDOWN].Y+3 >= r.Points[LEFTUP].Y && rectangle.Points[RIGHTDOWN].Y+3 <= r.Points[LEFTDOWN].Y &&
+		((rectangle.Points[LEFTUP].X <= r.Points[RIGHTDOWN].X && rectangle.Points[LEFTUP].X >= r.Points[LEFTDOWN].X) ||
+			(rectangle.Points[RIGHTUP].X >= r.Points[LEFTDOWN].X && rectangle.Points[RIGHTUP].X <= r.Points[RIGHTDOWN].X)) {
+		return DOWN
+	}
+	return -1
 }
 
 func (symbols Symbols) symbolsToByte() []byte {
@@ -352,81 +429,6 @@ func (round *Round) applyNames() {
 	}
 }
 
-func (player *Player) checkPosition(round *Round) {
-	for {
-		player.Car.recalculateBorders(false)
-		hit := false
-		for _, point := range player.Car.Borders.Points {
-			if point.X < 1 || point.X > mapWidth-nameTableWidth || point.Y < 1 || point.Y > mapHeight-1 {
-				// Hit the wall
-				player.Health -= FRONT * player.Car.Speed
-				hit = true
-				break
-			} else {
-				// Hit another car
-				for _, victim := range round.getAlivePlayers(player) {
-					if player.Car.Borders.intersects(&victim.Car.Borders) {
-						// Hit in the back
-						if (player.Car.Direction == RIGHT && victim.Car.Direction == LEFT) ||
-							(player.Car.Direction == LEFT && victim.Car.Direction == RIGHT) ||
-							(player.Car.Direction == UP && victim.Car.Direction == DOWN) ||
-							(player.Car.Direction == DOWN && victim.Car.Direction == UP) {
-							// Face to face
-							player.Health -= FRONT * (player.Car.Speed + victim.Car.Speed)
-						} else if (player.Car.Direction == RIGHT && victim.Car.Direction == UP) ||
-							(player.Car.Direction == LEFT && victim.Car.Direction == UP) ||
-							(player.Car.Direction == RIGHT && victim.Car.Direction == DOWN) ||
-							(player.Car.Direction == LEFT && victim.Car.Direction == DOWN) ||
-							(player.Car.Direction == UP && victim.Car.Direction == RIGHT) ||
-							(player.Car.Direction == UP && victim.Car.Direction == LEFT) ||
-							(player.Car.Direction == DOWN && victim.Car.Direction == RIGHT) ||
-							(player.Car.Direction == DOWN && victim.Car.Direction == LEFT) {
-							// Side hit
-							player.Health -= SIDE * player.Car.Speed
-						} else {
-							player.Health -= BACK * (player.Car.Speed - victim.Car.Speed)
-						}
-						hit = true
-						break
-					}
-				}
-				if hit {
-					break
-				}
-			}
-		}
-
-		if hit {
-			player.Car.recalculateBorders(true)
-			switch player.Car.Direction {
-			case RIGHT:
-				player.Car.Direction = LEFT
-			case LEFT:
-				player.Car.Direction = RIGHT
-			case UP:
-				player.Car.Direction = DOWN
-			case DOWN:
-				player.Car.Direction = UP
-			}
-			player.LastCrash = time.Now().Unix()
-		}
-
-		if player.Health <= 0 {
-			return
-		}
-
-		/*
-		 Because vertical symbols are 3x bigger, than horizontal, we need to slowdown recalculation of vertical objects
-		*/
-		slowerDown := int64(1)
-		if player.Car.Direction == UP || player.Car.Direction == DOWN {
-			slowerDown = 3
-		}
-
-		time.Sleep(time.Duration(slowerDown*100/player.Car.Speed*2) * time.Millisecond)
-	}
-}
-
 func (car *Car) recalculateBorders(crash bool) {
 	switch car.Direction {
 	case RIGHT:
@@ -464,24 +466,15 @@ func (car *Car) recalculateBorders(crash bool) {
 	}
 }
 
-func (player *Player) checkSpeed() {
-	for {
-		now := time.Now().Unix()
-		if now-player.LastCrash > player.Car.Speed*2 && player.Car.Speed < maxSpeed {
-			player.Car.Speed++
-		} else if now-player.LastCrash < 2 {
-			player.Car.Speed = 1
-		}
-		time.Sleep(1 % framesPerSecond * 100 * time.Millisecond)
-	}
-}
-
 func (player *Player) readDirection() {
 	if initTelnet(player.Conn) != nil {
 		return
 	}
 
 	for {
+		if player.Health <= 0 {
+			return
+		}
 		direction := make([]byte, 1)
 
 		// Read all possible bytes and try to find a sequence of:
@@ -543,17 +536,63 @@ func (player *Player) readDirection() {
 	}
 }
 
+/*
+Checks of the SIDE is safe
+*/
+func (player *Player) checkOkSide(players []*Player, side int) bool {
+	for _, p := range players {
+		if p.Name == player.Name {
+			continue
+		}
+		// If player nextTo someone from the SIDE - return false
+		if side == player.Car.Borders.nextTo(&p.Car.Borders) {
+			return false
+		}
+	}
+	return true
+}
+
+func (round *Round) getPlayersExcept(exceptions []Player) []*Player {
+	var rest []*Player
+	for i := range round.Players {
+		exception := false
+		for k := range exceptions {
+			if round.Players[i].Name == exceptions[k].Name {
+				exception = true
+			}
+		}
+		if !exception {
+			rest = append(rest, &round.Players[i])
+		}
+	}
+	return rest
+}
+
+func (round *Round) getRandomNonBotPlayerId() int {
+	for {
+		randId := rand.Intn(len(round.Players))
+		targetPlayer := round.Players[randId]
+		if targetPlayer.Health > 0 && !targetPlayer.Bot {
+			return randId
+		}
+	}
+}
+
 func (player *Player) moveBot(round *Round) {
 	for {
-		/*We need to find the best player to attack, based on:
-		* Less health
-		* Closest to us
-		 */
-
-		alivePlayers := round.getAlivePlayers(player)
-		targetPlayer := alivePlayers[rand.Intn(len(alivePlayers))]
+		targetPlayer := &round.Players[round.getRandomNonBotPlayerId()]
+		restPlayers := round.getPlayersExcept([]Player{*targetPlayer, *player})
 
 		for i := 0; i < 20; i++ {
+			// Do not hunt dead player
+			if targetPlayer.Health <= 0 {
+				break
+			}
+			if player.Health <= 0 {
+				return
+			}
+
+			// Navigate Bot based on centers of cars
 			myCenter := Point{
 				player.Car.Borders.Points[LEFTUP].X + (player.Car.Borders.Points[RIGHTUP].X-player.Car.Borders.Points[LEFTUP].X)/2,
 				player.Car.Borders.Points[LEFTUP].Y + (player.Car.Borders.Points[LEFTDOWN].Y-player.Car.Borders.Points[LEFTUP].Y)/2,
@@ -563,9 +602,10 @@ func (player *Player) moveBot(round *Round) {
 				targetPlayer.Car.Borders.Points[LEFTUP].Y + (targetPlayer.Car.Borders.Points[LEFTDOWN].Y-targetPlayer.Car.Borders.Points[LEFTUP].Y)/2,
 			}
 
+			// Chose closet way to the target
 			if targetCenter.X < myCenter.X {
 				// Target is left from us
-				if player.Car.Direction != RIGHT {
+				if player.Car.Direction != RIGHT && player.checkOkSide(restPlayers, LEFT) {
 					player.Car.Direction = LEFT
 				} else if targetCenter.Y < myCenter.Y {
 					player.Car.Direction = UP
@@ -574,15 +614,14 @@ func (player *Player) moveBot(round *Round) {
 				}
 			} else if targetCenter.X > myCenter.X {
 				// Target is right from us
-				if player.Car.Direction != LEFT {
+				if player.Car.Direction != LEFT && player.checkOkSide(restPlayers, RIGHT) {
 					player.Car.Direction = RIGHT
 				} else if targetCenter.Y < myCenter.Y {
 					player.Car.Direction = UP
 				} else {
 					player.Car.Direction = DOWN
 				}
-
-			} else if targetCenter.Y < myCenter.Y {
+			} else if targetCenter.Y < myCenter.Y && player.checkOkSide(restPlayers, UP) {
 				// Target is above us
 				if player.Car.Direction != DOWN {
 					player.Car.Direction = UP
@@ -591,7 +630,7 @@ func (player *Player) moveBot(round *Round) {
 				} else {
 					player.Car.Direction = LEFT
 				}
-			} else if targetCenter.Y > myCenter.Y {
+			} else if targetCenter.Y > myCenter.Y && player.checkOkSide(restPlayers, DOWN) {
 				// Target it below us
 				if player.Car.Direction != UP {
 					player.Car.Direction = DOWN
@@ -600,28 +639,120 @@ func (player *Player) moveBot(round *Round) {
 				} else {
 					player.Car.Direction = LEFT
 				}
-
 			}
+
 			time.Sleep(time.Duration(500 * time.Millisecond))
 		}
 	}
 }
 
-func (round *Round) getAlivePlayers(me *Player) []Player {
-	players := []Player{}
-	for _, p := range round.Players {
-		if me.Name == p.Name {
-			continue
-		} else if p.Health > 0 {
-			players = append(players, p)
+func (player *Player) checkPosition(round *Round) {
+	for {
+		if player.Health <= 0 {
+			return
 		}
+
+		player.Car.recalculateBorders(false)
+		hit := false
+		for _, point := range player.Car.Borders.Points {
+			if point.X < 1 || point.X > mapWidth-nameTableWidth || point.Y < 1 || point.Y > mapHeight-1 {
+				// Hit the wall
+				player.Health -= FRONT * player.Car.Speed
+				hit = true
+				break
+			} else {
+				// Hit another car
+				for _, victim := range round.Players {
+					if player.Name == victim.Name {
+						continue
+					}
+					if player.Car.Borders.intersects(&victim.Car.Borders) {
+						// Hit in the back
+						if (player.Car.Direction == RIGHT && victim.Car.Direction == LEFT) ||
+							(player.Car.Direction == LEFT && victim.Car.Direction == RIGHT) ||
+							(player.Car.Direction == UP && victim.Car.Direction == DOWN) ||
+							(player.Car.Direction == DOWN && victim.Car.Direction == UP) {
+							// Face to face
+							player.Health -= FRONT * (player.Car.Speed + victim.Car.Speed)
+						} else if (player.Car.Direction == RIGHT && victim.Car.Direction == UP) ||
+							(player.Car.Direction == LEFT && victim.Car.Direction == UP) ||
+							(player.Car.Direction == RIGHT && victim.Car.Direction == DOWN) ||
+							(player.Car.Direction == LEFT && victim.Car.Direction == DOWN) ||
+							(player.Car.Direction == UP && victim.Car.Direction == RIGHT) ||
+							(player.Car.Direction == UP && victim.Car.Direction == LEFT) ||
+							(player.Car.Direction == DOWN && victim.Car.Direction == RIGHT) ||
+							(player.Car.Direction == DOWN && victim.Car.Direction == LEFT) {
+							// Side hit
+							player.Health -= SIDE * player.Car.Speed
+						} else {
+							player.Health -= BACK * (player.Car.Speed - victim.Car.Speed)
+						}
+						hit = true
+						break
+					}
+				}
+				if hit {
+					break
+				}
+			}
+		}
+
+		if hit {
+			player.Car.recalculateBorders(true)
+			switch player.Car.Direction {
+			case RIGHT:
+				player.Car.Direction = LEFT
+			case LEFT:
+				player.Car.Direction = RIGHT
+			case UP:
+				player.Car.Direction = DOWN
+			case DOWN:
+				player.Car.Direction = UP
+			}
+			player.LastCrash = time.Now().Unix()
+		}
+
+		/*
+		 Because vertical symbols are 3x bigger, than horizontal, we need to slowdown recalculation of vertical objects
+		*/
+		slowerDown := int64(1)
+		if player.Car.Direction == UP || player.Car.Direction == DOWN {
+			slowerDown = 3
+		}
+
+		time.Sleep(time.Duration(slowerDown*100/player.Car.Speed*2) * time.Millisecond)
 	}
-	return players
+}
+
+func (player *Player) checkSpeed() {
+	for {
+		if player.Health <= 0 {
+			return
+		}
+		now := time.Now().Unix()
+		if now-player.LastCrash > player.Car.Speed*2 && player.Car.Speed < maxSpeed {
+			player.Car.Speed++
+		} else if now-player.LastCrash < 2 {
+			player.Car.Speed = 1
+		}
+		time.Sleep(1 % framesPerSecond * 100 * time.Millisecond)
+	}
+}
+
+func (player *Player) checkHealth() {
+	for {
+		if player.Health <= 0 {
+			player.Health = 0
+			player.Color = BOLD
+			return
+		}
+		time.Sleep(1 % framesPerSecond * 100 * time.Millisecond)
+	}
 }
 
 func (round *Round) collisionChecker() {
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(1)
 	for i := range round.Players {
 		if round.Players[i].Bot {
 			go round.Players[i].moveBot(round)
@@ -630,54 +761,9 @@ func (round *Round) collisionChecker() {
 		}
 		go round.Players[i].checkPosition(round)
 		go round.Players[i].checkSpeed()
+		go round.Players[i].checkHealth()
 	}
 	wg.Wait()
-}
-
-func applyHealth(round *Round, activeMap []Symbol) {
-	for line, player := range round.Players {
-		if player.Health < 0 {
-			player.Health = 0
-		}
-
-		health := []byte(fmt.Sprintf("%3d", player.Health))
-		for i, char := range health {
-			activeMap[(line+1)*mapWidth+(mapWidth-3)-len(health)+i] = Symbol{player.Color, []byte{char}}
-		}
-	}
-}
-
-func applyCars(round *Round, activeMap []Symbol) {
-	for _, player := range round.Players {
-		if player.Health <= 0 {
-			continue
-		}
-		charPosX, charPosY := 0, 0
-		for i := 0; i < len(cars[player.Car.Direction]); i++ {
-			var chars []byte
-			if cars[player.Car.Direction][i] == byte('\n') {
-				charPosY++
-				charPosX = 0
-				continue
-			} else if cars[player.Car.Direction][i] == 226 {
-				/*
-				 This means extended ASCII is used. After 226 2 bytes must follow
-				*/
-				chars = []byte{cars[player.Car.Direction][i], cars[player.Car.Direction][i+1], cars[player.Car.Direction][i+2]}
-				i += 2
-			} else if cars[player.Car.Direction][i] == 194 {
-				/*
-				 This means extended ASCII is used. After 194 1 bytes must follow
-				*/
-				chars = []byte{cars[player.Car.Direction][i], cars[player.Car.Direction][i+1]}
-				i++
-			} else {
-				chars = []byte{cars[player.Car.Direction][i]}
-			}
-			activeMap[(player.Car.Borders.Points[LEFTUP].Y+charPosY)*mapWidth+player.Car.Borders.Points[LEFTUP].X+charPosX] = Symbol{player.Color, chars}
-			charPosX++
-		}
-	}
 }
 
 func (round *Round) checkGameOver() {
@@ -697,6 +783,7 @@ func (round *Round) checkGameOver() {
 	secondsLeft := round.LastStateChange.Unix() + maxRoundRunningTimeSec - time.Now().Unix()
 	if humans == deads || secondsLeft <= 0 {
 		round.State = FINISHED
+		fmt.Println("Round has changed to the state FINISHED")
 	}
 }
 
@@ -710,8 +797,8 @@ func (round Round) start() {
 		activeFrameBuffer := make(Symbols, len(round.FrameBuffer))
 		copy(activeFrameBuffer, round.FrameBuffer)
 
-		applyHealth(&round, activeFrameBuffer)
-		applyCars(&round, activeFrameBuffer)
+		round.applyHealth(activeFrameBuffer)
+		round.applyCars(activeFrameBuffer)
 
 		round.writeToAllPlayers(activeFrameBuffer.symbolsToByte(), false)
 
@@ -720,7 +807,6 @@ func (round Round) start() {
 			round.over()
 			return
 		}
-
 		time.Sleep(1 % framesPerSecond * 100 * time.Millisecond)
 	}
 
